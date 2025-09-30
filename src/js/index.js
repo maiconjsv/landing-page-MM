38d5938db0202a1264bec2d9e32061659af0d307
Maicon <maiconjnsv@gmail.com>
2025-09-29 22:57:59 -0300

    finalizei o esqueleto da página já om alguma responsividade, faltando agora demais ajustes responsivos e dinamicos om javascript

--- src/js/index.js
+++ src/js/index.js
@@ -23,5 +23,29 @@ window.addEventListener('scroll', () =>{
 })
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    //Seleciona todos os botões de pergunta
+    var botoesPergunta = document.querySelectorAll('.pergunta');
 
+    //Itera sobre cada botão para adicionar um 'ouvinte' de evento
+    botoesPergunta.forEach(function(botao) {
+        botao.addEventListener('click', function() {
+            //Adiciona/Remove a classe 'active' ao botão
+            this.classList.toggle('active');
 
+            //Seleciona o painel de resposta que está logo após o botão
+            var painel = this.nextElementSibling;
+
+            //Verifica se o painel está visível (se tem altura)
+            if (painel.style.maxHeight) {
+                // Se estiver visível, esconde
+                painel.style.maxHeight = null;
+                painel.style.padding = '0 18px'; // Remove o padding para o fechamento
+            } else {
+                // Se estiver escondido, mostra, definindo a altura para o tamanho real do conteúdo
+                painel.style.maxHeight = painel.scrollHeight + "px";
+                painel.style.padding = '10px 18px 20px 18px'; // Define o padding
+            }
+        });
+    });
+});
